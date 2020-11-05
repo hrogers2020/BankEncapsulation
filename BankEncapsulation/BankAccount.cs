aa1748b0130ac48581d9d124a5102d19eeca3535
Hampton Rogers <rogershampton22@gmail.com>
2020-11-04 22:51:27 -0600

    Add project files.

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        public BankAccount()
+        {
+        }
+
+        private double balance = 0;
+
+        //define a method named Deposit that will accept a double 
+        //and store that value in the balance field
+
+            //Encapsulation
+        public void Deposit(double amount)
+        {
+            balance = amount;
+        }
+        //Define a method named GetBalance that
+        //will return the amount stored in the balance field
+        public string GetBalance()
+        {
+            return $"${balance}";
+        }
+    }
+}
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hamptonsAccount = new BankAccount();
+            Console.WriteLine("How much would you like to deposit?");
+
+            var amountToDeposit = double.Parse(Console.ReadLine());
+
+            hamptonsAccount.Deposit(amountToDeposit);
+
+            Console.WriteLine($"Thank you! Your balance is now {hamptonsAccount.GetBalance()}");
+        }
+    }
+}
